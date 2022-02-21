62789d183ebb78ec481d24e46d92928f60d0cbce
Stefzockt <moser.stefan02@gmail.com>
2022-02-21 18:41:04 +0100

    Start Coffe and Time Coffe added

--- Scripts/myserver.py
+++ Scripts/myserver.py
@@ -14,7 +14,7 @@ async def echo(websocket, path):
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
 
-start_server = websockets.serve(echo, "192.168.2.113", PORT)
+start_server = websockets.serve(echo, "localhost", PORT)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
